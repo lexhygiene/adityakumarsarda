e1a7ab6f28ab2ee39a6d33957697c2de47b17744
Your Name <you@example.com>
2026-05-13 15:30:22 +0530

    feat: add global ScrollToTop component to fix navigation scroll issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Home from './pages/Home';
 import Blog from './pages/Blog';
 import BlogPost from './pages/BlogPost';
 import StudioPage from './pages/Studio';
+import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -38,6 +38,7 @@ export default function Blog() {
     };
 
     fetchPosts();
+    window.scrollTo(0, 0);
   }, []);
 
   return (
